6093656151e363cb1f5d89cac04355dc2b8a664b
syedaa-ayesha <syedaa923@gmail.com>
2025-01-24 16:27:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import Picker from './components/Picker';
+import Emojipicker from './components/Emojipicker';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Picker /> */}
+      <Emojipicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Emojipicker.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+const Emojipicker = () => {
+const [ShowEmoji, setShowEmoji] = useState([]);
+const [emojiList] = useState([
+    "😀", "😁", "😂", "🤣", "😃", 
+    "😄", "😅", "😆", "😉", "😊",
+    "😍", "😘", "😗", "😙", "😚", 
+    "😎", "🤓", "🧐", "😕", "😢", 
+    "😭", "😡", "😠", "🤬", "😱", 
+    "😴", "😪", "🤤", "😵", "🤯", 
+    "🤔", "🤗", "🤨", "😶", "🙄", 
+    "👍", "👎", "👌", "✌️", "🤟", 
+    "👏", "🙌", "🙏", "💪", "🤝", 
+    "❤️", "💔", "💖", "💞", "💕",
+    "🎉", "🎊", "🎂", "🍰", "🎈", 
+    "🏆", "⚽", "🏀", "🎾", "🏓", 
+    "🚗", "✈️", "🚢", "🚀", "🏝️",
+    "🌞", "🌈", "⭐", "🔥", "💡",
+  ])
+
+let addEmoji = (emj , index)=> {
+    setShowEmoji((pre)=>pre + emj);
+}
+  return (
+   <>
+   <h1>Emoji Picker</h1>
+   <div className="container">
+     {
+        emojiList.map((emj, index)=>
+           <span className='span' key ={index}  onClick={()=>addEmoji(emj, index)}
+           style={{fontSize: '22px', cursor: 'pointer'}}> {emj}</span>
+        )
+    } 
+   </div>
+   <div className="showEmoji">
+    {ShowEmoji  && <span>{ShowEmoji}</span>}
+   </div>
+   
+   </>
+  )
+}
+
+export default Emojipicker
